0f6d3df6e9f39a30b08df1b32eefb738121a7e3f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-12-19 13:15:57 +0530

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const fetchData = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = res.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
